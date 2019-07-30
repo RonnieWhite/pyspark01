17d0328b83b202ab826d10292c4ad9839797ef3f
R.White <15018408620@163.com>
2019-07-30 23:49:23 +0800

    spark streaming test.

--- pyspark201907/sparkstreaming0730.py
+++ pyspark201907/sparkstreaming0730.py
@@ -1,5 +1,8 @@
 import sys
-if __name__=='__main__':
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage sparkstreaming0730.py <directory>", file=sys.stderr)
         sys.exit(-1)
@@ -7,8 +10,8 @@ if __name__=='__main__':
     conf = SparkConf().setMaster('localhost[2]').setAppName('streaming0730')
     sc = SparkContext(conf)
     ssc = StreamingContext(sc, 5)
-    lines =ssc.textFileStream(sys.argv[1])
-    counts = lines.flatMap(lambda line: line,split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a,b: a+b)
-    countes.pprint()
+    lines = ssc.textFileStream(sys.argv[1])
+    counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+    counts.pprint()
     ssc.start()
     ssc.awaittermination()
