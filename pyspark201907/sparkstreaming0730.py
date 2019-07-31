fec3a96e4e817f509761f13f8d80f69b73e0432d
R.White <15018408620@163.com>
2019-08-01 00:08:44 +0800

    streaming demo1

--- pyspark201907/sparkstreaming0730.py
+++ pyspark201907/sparkstreaming0730.py
@@ -1,5 +1,5 @@
 import sys
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
 if __name__ == '__main__':
@@ -7,11 +7,11 @@ if __name__ == '__main__':
         print("Usage sparkstreaming0730.py <directory>", file=sys.stderr)
         sys.exit(-1)
 
-    conf = SparkConf().setMaster('localhost[2]').setAppName('streaming0730')
-    sc = SparkContext(conf)
+    sc = SparkContext(appName='streaming0730')
     ssc = StreamingContext(sc, 5)
-    lines = ssc.textFileStream(sys.argv[1])
+    # lines = ssc.textFileStream(sys.argv[1])
+    lines = ssc.socketTextStream('127.0.0.1', 9999)
     counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
     counts.pprint()
     ssc.start()
-    ssc.awaittermination()
+    ssc.awaitTermination()
